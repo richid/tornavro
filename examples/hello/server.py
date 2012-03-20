905a2b0ebc2a7e825f93fe1411dc598524dfd843
Rich Schumacher <rich.schu@gmail.com>
2012-03-20 08:56:36 -0700

    Make the hello protocol example more user-friendly

--- examples/hello/server.py
+++ examples/hello/server.py
@@ -3,9 +3,13 @@ import os
 import avro.protocol
 import tornado.web
 import tornado.ioloop
+from tornado.options import define, options
 
 import tornavro.server
 import tornavro.responder
+
+
+define('port', default=8888, help='Listen on this port')
 
 
 class HelloResponder(tornavro.responder.Responder):
@@ -13,10 +17,12 @@ class HelloResponder(tornavro.responder.Responder):
         return 'Hello, %s' % name
 
 
+tornado.options.parse_command_line()
+
 proto = open(os.path.join(os.path.dirname(__file__), 'hello.avpr')).read()
 proto = avro.protocol.parse(proto)
 responder = HelloResponder(proto)
 
 server = tornavro.server.AvroServer(responder)
-server.listen(8888)
+server.listen(options.port)
 tornado.ioloop.IOLoop.instance().start()
